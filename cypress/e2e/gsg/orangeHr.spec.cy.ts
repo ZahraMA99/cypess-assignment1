e7b2fca3358ae50e16e34624bc2e7d89dfab2ea2
baraaqel <baraaqel1998@gmail.com>
2023-09-24 22:44:12 +0300

    .

--- cypress/e2e/gsg/orangeHr.spec.cy.ts
+++ cypress/e2e/gsg/orangeHr.spec.cy.ts
@@ -4,19 +4,28 @@ import loginPage from "../../pageObjects/loginPage";
 const loginObj: loginPage = new loginPage();
 const addEmployeeObj: addEmployee = new addEmployee();
 
+// describe 
 describe("login", () => {
   beforeEach(function () {
     cy.intercept("/web/index.php/dashboard/index").as("loginPage");
     cy.visit("/web/index.php/auth/login");
     loginObj.login("Admin", "admin123");
   });
-  it("Validate locations api", () => {
+
+// validate locations api
+  /*it("Validate locations api", () => {
     cy.request("/web/index.php/api/v2/dashboard/employees/locations").then(
       (response) => {
         expect(response).to.have.property("status", 200);
       }
     );
-  })
+  });*/
+
+  // add employee test case 
+  it("add employee", () => {
+    addEmployeeObj.addNewEmployee("Zahra", "Mahmoud", "Aqel", "zahra99");
+  });
+
 });
 
 
--- cypress/pimObject/addEmployee.ts
+++ cypress/pimObject/addEmployee.ts
@@ -5,14 +5,18 @@ class addEmployee {
     AddEmp: () => cy.get(".oxd-button--secondary"),
     EmployeeInputName: () => cy.get(".--name-grouped-field"),
     saveNewEmp: () => cy.get('button[type="submit"]'),
-  };
+    createLoginSwitch: () => cy.get('.oxd-switch-wrapper'),
+    userName: () => cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.orangehrm-employee-container > div.orangehrm-employee-form > div:nth-child(4) > div > div:nth-child(1) > div > div:nth-child(2) > input'),
+    };
 // Actions
-  addNewEmployee(firstName: string, MiddleName: string, LastName: string) {
+  addNewEmployee(firstName: string, MiddleName: string, LastName: string, userName: string) {
     this.elements.MainMenuItems().contains("PIM").click();
     this.elements.AddEmp().eq(1).click();
     this.elements.EmployeeInputName().children().eq(0).type(firstName);
     this.elements.EmployeeInputName().children().eq(1).type(MiddleName);
     this.elements.EmployeeInputName().children().eq(2).type(LastName);
+    this.elements.createLoginSwitch().click();
+    this.elements.userName().type(userName);
   }
 }
 export default addEmployee;
