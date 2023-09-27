9eec4c892f229cefde163ff636637803b0b78d67
baraaqel <baraaqel1998@gmail.com>
2023-09-28 00:05:15 +0300

    ..

--- cypress/e2e/gsg/orangeHr.spec.cy.ts
+++ cypress/e2e/gsg/orangeHr.spec.cy.ts
@@ -4,18 +4,11 @@ import loginPage from "../../pageObjects/loginPage";
 const loginObj: loginPage = new loginPage();
 const addEmployeeObj: addEmployee = new addEmployee();
 
-// describe 
-describe("login", () => {
+// describe
+describe("check login", () => {
   beforeEach(function () {
-    cy.intercept("/web/index.php/dashboard/index").as("loginPage");
     cy.visit("/web/index.php/auth/login");
     loginObj.login("Admin", "admin123");
   });
-
-  // add employee test case 
-  it("add employee", () => {
-    addEmployeeObj.addNewEmployee("mela", "sultan", "sh", "meme", "123456789Xx", "123456789Xx");
-    //addEmployeeObj.checkEmployeeName("Zahra", "Aqel");
-  });
-
 });
+
