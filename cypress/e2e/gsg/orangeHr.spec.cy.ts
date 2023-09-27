374b311d37993090c6c2064db3fe16c5fc321a41
baraaqel <baraaqel1998@gmail.com>
2023-09-27 14:02:51 +0300

    ..

--- cypress/e2e/gsg/orangeHr.spec.cy.ts
+++ cypress/e2e/gsg/orangeHr.spec.cy.ts
@@ -1,4 +1,4 @@
-import addEmployee from "../../pimObject/addEmployee";
+import addEmployee from "../../pageObjects/addEmployee";
 import loginPage from "../../pageObjects/loginPage";
 
 const loginObj: loginPage = new loginPage();
@@ -12,19 +12,10 @@ describe("login", () => {
     loginObj.login("Admin", "admin123");
   });
 
-// validate locations api
-  /*it("Validate locations api", () => {
-    cy.request("/web/index.php/api/v2/dashboard/employees/locations").then(
-      (response) => {
-        expect(response).to.have.property("status", 200);
-      }
-    );
-  });*/
-
   // add employee test case 
   it("add employee", () => {
-    addEmployeeObj.addNewEmployee("Zahra", "Mahmoud", "Aqel", "zahramh7", "123456789Xx", "123456789Xx");
-    addEmployeeObj.checkEmployeeName("Zahra", "Aqel");
+    addEmployeeObj.addNewEmployee("mela", "sultan", "sh", "mmila", "123456789Xx", "123456789Xx", "melo");
+    //addEmployeeObj.checkEmployeeName("Zahra", "Aqel");
   });
 
 });
--- cypress/pageObjects/addEmployee.ts
+++ cypress/pageObjects/addEmployee.ts
@@ -6,22 +6,14 @@ class addEmployee {
     EmployeeInputName: () => cy.get(".--name-grouped-field"),
     saveNewEmp: () => cy.get('button[type="submit"]'),
     createLoginSwitch: () => cy.get(".oxd-switch-wrapper"),
-    userName: () =>
-      cy.get(
-        "#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.orangehrm-employee-container > div.orangehrm-employee-form > div:nth-child(4) > div > div:nth-child(1) > div > div:nth-child(2) > input"
-      ),
-    password: () =>
-      cy.get(
-        "#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.orangehrm-employee-container > div.orangehrm-employee-form > div.oxd-form-row.user-password-row > div > div.oxd-grid-item.oxd-grid-item--gutters.user-password-cell > div > div:nth-child(2) > input"
-      ),
-    confirmPassword: () =>
-      cy.get(
-        "#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.orangehrm-employee-container > div.orangehrm-employee-form > div.oxd-form-row.user-password-row > div > div:nth-child(2) > div > div:nth-child(2) > input"
-      ),
+    userName: () => cy.get("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.orangehrm-employee-container > div.orangehrm-employee-form > div:nth-child(4) > div > div:nth-child(1) > div > div:nth-child(2) > input"),
+    password: () => cy.get("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.orangehrm-employee-container > div.orangehrm-employee-form > div.oxd-form-row.user-password-row > div > div.oxd-grid-item.oxd-grid-item--gutters.user-password-cell > div > div:nth-child(2) > input"),
+    confirmPassword: () => cy.get("#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div > form > div.orangehrm-employee-container > div.orangehrm-employee-form > div.oxd-form-row.user-password-row > div > div:nth-child(2) > div > div:nth-child(2) > input"),
     statusEnabled: () => cy.get(".--label-right"),
     statusDisabled: () => cy.get(".--label-left"),
     save: () => cy.get(".oxd-form-actions"),
-    assertEmployeeName: () => cy.get(".orangehrm-edit-employee-name"),
+    //assertEmployeeName: () => cy.get(".orangehrm-edit-employee-name"),
+    //employeeNickName: () => cy.get("."),
   };
   // Actions
   addNewEmployee(
@@ -30,7 +22,8 @@ class addEmployee {
     lastName: string,
     userName: string,
     password: string,
-    confirmPassword: string
+    confirmPassword: string,
+    nickName: string
   ) {
     this.elements.MainMenuItems().contains("PIM").click();
     this.elements.AddEmp().eq(1).click();
@@ -44,12 +37,13 @@ class addEmployee {
     //this.elements.statusEnabled().click();
     //this.elements.statusDisabled().click();
     this.elements.save().children().eq(2).click();
+    //this.elements.employeeNickName().children().eq(1).type(nickName);
   }
   //- Write an assertion for the Employee name text shown in the header !! not complete yet..
-  checkEmployeeName(firstName: string, LastName: string) {
+  /*checkEmployeeName(firstName: string, LastName: string) {
     this.elements
       .assertEmployeeName()
       .should("have.string", `${firstName} ${LastName}`);
-  }
+  }*/
 }
 export default addEmployee;
